8816aab6f5383f6dab5e4c34048456f998c4e734
Charley Robinson <charley@tokbox.com>
2013-09-12 16:00:48 -0700

    Clean up, slob

--- build-ios.sh
+++ build-ios.sh
@@ -11,11 +11,11 @@ gclient sync
 perl -i -wpe "s/target\_os \= \[\'mac\'\]/target\_os \= \[\'ios\', \'mac\']/g" .gclient
 gclient sync
 cd trunk
-export GYP_DEFINES="build_with_libjingle=1 build_with_chromium=0 libjingle_objc=1"
+export GYP_DEFINES="build_with_libjingle=1 build_with_chromium=0 libjingle_objc=1 OS=ios target_arch=armv7"
 export GYP_GENERATORS="ninja"
-export GYP_DEFINES="$GYP_DEFINES OS=ios target_arch=armv7"
 export GYP_GENERATOR_FLAGS="output_dir=out_ios"
 export GYP_CROSSCOMPILE=1
 gclient runhooks
+ninja -C out_ios/Debug -t clean
 ninja -C out_ios/Debug libjingle_peerconnection_objc_test
 
