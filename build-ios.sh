322d26b7d54701871d00ac5f9c66dcc57afe06da
Charley Robinson <charley@tokbox.com>
2013-09-13 16:23:28 -0700

    Make a huge static library

--- build-ios.sh
+++ build-ios.sh
@@ -1,4 +1,4 @@
-#!/bin/sh -xe
+#!/bin/bash -xe
 
 #
 # This script automates the build process described by webrtc:
@@ -18,4 +18,24 @@ export GYP_CROSSCOMPILE=1
 gclient runhooks
 ninja -C out_ios/Debug -t clean
 ninja -C out_ios/Debug libjingle_peerconnection_objc_test
+
+AR=`xcrun -f ar`
+PWD=`pwd`
+ROOT=$PWD
+LIBS_OUT=`find $PWD/out_ios/Debug -d 1 -name '*.a'`
+FATTYCAKES_OUT=out.huge
+mkdir -p $FATTYCAKES_OUT
+cd $FATTYCAKES_OUT
+for LIB in $LIBS_OUT
+do
+    $AR -x $LIB
+done
+$AR -q libfattycakes.a *.o
+
+cd $ROOT
+
+REVISION=`svn info $BRANCH | grep Revision | cut -f2 -d: | tr -d ' '`
+echo "WEBRTC_REVISION=$REVISION" > build.properties
+
+
 
